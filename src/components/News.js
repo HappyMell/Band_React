2c77fd0b075d6e8d9cf3db54e369aadeca0dd007
Mellisa Madrid <mellisamadrid@hotmail.com>
2019-09-12 18:15:52 +0100

    animations

--- src/components/News.js
+++ src/components/News.js
@@ -1,35 +1,60 @@
 import React, { Component } from "react";
-import firebase from "firebase";
-import firebaseConfig from "../firebase-config";
-
-firebase.initializeApp(firebaseConfig);
+import firebase from "../firebase";
+import Fade from "react-reveal/Fade";
+import { Accordion, Card, Button } from "react-bootstrap";
 
 class News extends Component {
   state = {
-    img: null
+    news: []
   };
 
   componentDidMount() {
-    /*
-    const images = firebase
-      .storage()
-      .ref()
-      .child("news/");
-    const image = images.child("hearts.jpg");
-    image.getDownloadURL().then(url => {
+    const newsRef = firebase.database().ref("news");
+    newsRef.on("value", snapshot => {
+      let news = snapshot.val();
+
+      let newState = [];
+      for (let newie in news) {
+        newState.push({
+          id: newie,
+          title: news[newie].title,
+          image: news[newie].image,
+          details: news[newie].details
+        });
+      }
       this.setState({
-        img: url
+        news: newState
       });
     });
-    console.log(image.getDownloadURL());
-    */
   }
 
   render() {
     return (
       <div className='news'>
-        <p>Hello</p>
-        <img src={this.state.img} />
+        {this.state.news.map((newbie, index) => {
+          return (
+            <div key={index} className='news_info'>
+              <Accordion defaultActiveKey='0'>
+                <Card>
+                  <Fade bottom>
+                    <Card.Header>
+                      <p>{newbie.title}</p>
+                      <Accordion.Toggle as={Button} variant='link' eventKey='0'>
+                        +
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey='0'>
+                      <Card.Body>
+                        <img src={newbie.image} />
+                        <p>{newbie.details}</p>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Fade>
+                </Card>
+              </Accordion>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-var firebaseConfig = {
+import firebase from "firebase";
+
+const config = {
   apiKey: "AIzaSyDJRN4Rql-zv1e_EH-1lcL9i6P-77XYq1Y",
   authDomain: "band-bc106.firebaseapp.com",
   databaseURL: "https://band-bc106.firebaseio.com",
@@ -7,4 +9,6 @@ var firebaseConfig = {
   messagingSenderId: "618022200565",
   appId: "1:618022200565:web:fee861fc96646ee6dfd1bc"
 };
-export default firebaseConfig;
+
+firebase.initializeApp(config);
+export default firebase;
